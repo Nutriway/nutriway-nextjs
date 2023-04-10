d913b99340872c41f8bc5b810242c1f07c671c36
Andre Ribeiro <andresmribeiro@gmail.com>
2023-04-10 23:16:07 +0100

    same thing as the client but the server version

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,14 @@ import Potato from "@/components/Potatoes/Potato";
 import { cookies } from "next/headers";
 import { serverFetcher } from "@/lib/fetchers/serverFetcher";
 
-const getData = async (id: number) => {
+const getAllAppointments = async () => {
+    return serverFetcher({
+        url: `/appointments`,
+        method: "get"
+    });
+};
+
+const getAppointmentFromId = async (id: number) => {
     const data = await serverFetcher({
         url: `/appointments?populate[appointment_payment][populate]=*&populate[nutritionist_availability][populate]=*&populate[client][populate]=*&populate[appointment_result][populate]=*&pagination[pageSize]=2000&filters[id][$eq]=${id}`,
         method: "get"
@@ -18,7 +25,8 @@ const content = {
 
 export default async function Home() {
     // Start using URL parameters for stuff like pagination
-    const jwt = await getData(1);
+    const appointment = await getAllAppointments();
+    const jwt = await getAppointmentFromId(appointment?.data[0].id);
     return (
         <div>
             <Potato></Potato>
