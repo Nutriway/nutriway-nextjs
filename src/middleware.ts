f5ffc20e02247a4632696137e2b89076a11b3f87
Andre Ribeiro <andresmribeiro@gmail.com>
2023-05-08 12:50:01 +0100

    feat: cannot go to login page twice

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,14 +4,18 @@ const PUBLIC_FILE = /\.(.*)$/;
 
 export default async function middleware(req: NextRequest) {
     const { pathname } = req.nextUrl;
-    if (pathname.startsWith('/_next') || pathname.startsWith('/api') || pathname.startsWith('/static') || pathname.startsWith('/signin') || pathname.startsWith('/login') || PUBLIC_FILE.test(pathname)) {
+    if (pathname.startsWith('/_next') || pathname.startsWith('/api') || pathname.startsWith('/static') || PUBLIC_FILE.test(pathname)) {
         return NextResponse.next();
     }
 
     const jwt = req.cookies.get(process.env.COOKIE_NAME as string);
 
-    if (!jwt) {
-        req.nextUrl.pathname = '/login'; // change this to /signin as we want to redirect to the signin page if we have no jwt
+    if (!(jwt || pathname.startsWith('/login') || pathname.startsWith('/register'))) {
+        req.nextUrl.pathname = '/login';
+        return NextResponse.redirect(req.nextUrl);
+    }
+    if (jwt && (pathname.startsWith('/login') || pathname.startsWith('/register'))) {
+        req.nextUrl.pathname = '/intro';
         return NextResponse.redirect(req.nextUrl);
     }
     try {
