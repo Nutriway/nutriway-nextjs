32ff6e5fbd370d96455d604c094003777f8c059e
Andre Ribeiro <andresmribeiro@gmail.com>
2023-05-29 00:43:47 +0100

    chore: removed console log

--- src/app/@client/(content)/info/page.tsx
+++ src/app/@client/(content)/info/page.tsx
@@ -2,16 +2,19 @@ import { serverFetcher } from '@/lib/fetchers/serverFetcher';
 import { User } from '@/types/User';
 import UserInfoForm from '@/components/Forms/UserInfoForm';
 
-async function getUser(): Promise<User> {
-    const user = await serverFetcher<User>({ url: '/users/me', method: 'get' });
-    console.log(user);
-    return user!; // this is always returned
+async function getUser(): Promise<User | undefined> {
+    return serverFetcher<User>({ url: '/users/me', method: 'get' });
 }
 
 export default async function Info() {
     const user = await getUser();
 
     return (
-        <UserInfoForm currentUser={user} appointmentDate={new Date()} nutritionistAvailability={1} nutritionistId={1} />
+        <UserInfoForm
+            currentUser={user!} // user is always returned
+            appointmentDate={new Date()}
+            nutritionistAvailability={1}
+            nutritionistId={1}
+        />
     );
 }
