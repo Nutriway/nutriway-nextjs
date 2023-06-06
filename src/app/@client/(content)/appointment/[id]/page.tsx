5572d58e87258138506c5a364ddb3561a29a4a89
Andre Ribeiro <andresmribeiro@gmail.com>
2023-06-06 17:06:29 +0100

    feat: see upcoming consultation, check details, button to join consultation

---
+++ src/app/@client/(content)/appointment/[id]/page.tsx
@@ -0,0 +1,27 @@
+import ClientDetails from '@/components/CRUD/ClientDetails';
+import { serverFetcher } from '@/lib/fetchers/serverFetcher';
+import { SingleStrapiResponse } from '@/types/StrapiResponse';
+import { Appointment } from '@/types/Appointment';
+import React from 'react';
+import NotFound from '@/components/Errors/404';
+
+type AppointmentDetailsParams = {
+    params: { id: string };
+};
+
+async function getInfo(id: string) {
+    return serverFetcher<SingleStrapiResponse<Appointment>>({
+        url: `/appointments/${id}?populate[client][populate]`,
+        method: 'get',
+    });
+}
+
+export default async function AppointmentDetails({ params }: AppointmentDetailsParams) {
+    const info = await getInfo(params.id);
+
+    if (!info.data) {
+        return <NotFound />;
+    }
+
+    return <ClientDetails info={info.data} />;
+}
